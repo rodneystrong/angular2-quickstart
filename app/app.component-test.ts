75b73579a53bd8e3543079926aa520b81630692c
aleeex <akang2@gmail.com>
2016-09-02 17:26:35 -0600

    successfully added a new component and got it to live load

---
+++ app/app.component-test.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component ({
+  selector: 'my-test',
+  template: '<h2>What are you doing to accomplish that?</h2>'
+})
+
+export class TestComponent{}
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,11 +2,12 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
+import {TestComponent} from './app.component-test';
 
 @NgModule ({
   imports: [BrowserModule],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, TestComponent],
+  bootstrap: [AppComponent, TestComponent]
 })
 
 export class AppModule{}
