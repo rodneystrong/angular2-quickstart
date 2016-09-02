80eef421247190dba6b31d5966abbca893c78e10
aleeex <akang2@gmail.com>
2016-09-02 00:28:01 -0600

    ran npm start and got errors. misspelled platformBrowserDynamic in main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app.module';
 
-const platform = platFormBrowserDynamic();
+const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
