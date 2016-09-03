eedcfaad606ac5d653d28cd99406460d7878c801
aleeex <akang2@gmail.com>
2016-09-02 22:23:51 -0600

    getting brnanch ready for rebasing with practice1

--- app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  template: '<h1>My first time go easy</h1>'
-})
-
-//im guessing the line below corresponds 
-export class AppComponent{}
